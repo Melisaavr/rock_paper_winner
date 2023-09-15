5785dbe4e200c9abf85b3ea96319ad9697c8ec04
Melisaavr <melisadayicikk@gmail.com>
2023-09-15 11:46:11 +0100

    edit play again button and add function to generate a random choice by the computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ let playerScore = 0;
 let computerScore = 0;
 let currentRound = 1;
 let gameOver = false;
+
+// Hide the "Play Again" button initially
+document.getElementById("playAgain").style.display = "none";
+
+// Function to generate a random computer choice
+function computerChoice() {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
