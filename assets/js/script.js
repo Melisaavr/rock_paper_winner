15acce95489a8b2812d05c6e1be8bfed84142f75
Melisaavr <melisadayicikk@gmail.com>
2023-09-15 11:44:15 +0100

    define choices and initial scores

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Define an array of game choices
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// Initialize player and computer scores, current round, and game state
+let playerScore = 0;
+let computerScore = 0;
+let currentRound = 1;
+let gameOver = false;
